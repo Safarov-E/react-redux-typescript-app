2734a4cdff9b0ad46acb6e145be83f7a2d287e55
safarov-e <elkhan.safarov10@yandex.com>
2021-03-08 11:45:25 +0500

    Рефакторинг

--- src/store/reducers/userReducer.ts
+++ src/store/reducers/userReducer.ts
@@ -1,37 +1,11 @@
-interface UserState {
-    users: any[];
-    loading: boolean;
-    error: null | string;
-}
-
-enum UserActionTypes {
-    FETCH_USERS = 'FETCH_USERS',
-    FETCH_USERS_SUCCESS = 'FETCH_USERS_SUCCESS',
-    FETCH_USERS_ERROR = 'FETCH_USERS_ERROR'
-}
-
-interface FetchUsersAction {
-    type: UserActionTypes.FETCH_USERS;
-}
-
-interface FetchUsersSuccessAction {
-    type: UserActionTypes.FETCH_USERS_SUCCESS;
-    payload: any[];
-}
-
-interface FetchUsersErrorAction {
-    type: UserActionTypes.FETCH_USERS_ERROR;
-    payload: string;
-}
-
-type UserAction = FetchUsersAction | FetchUsersSuccessAction | FetchUsersErrorAction
+import {UserAction, UserActionTypes, UserState} from '../../types/todo'
 
 const initialState: UserState = {
     users: [],
     loading: false,
     error: null
 }
-export const userReducer = (state = initialState, action): UserState => {
+export const userReducer = (state = initialState, action: UserAction): UserState => {
     switch(action.type) {
         case UserActionTypes.FETCH_USERS:
             return {loading: true, error: null, users: []}
