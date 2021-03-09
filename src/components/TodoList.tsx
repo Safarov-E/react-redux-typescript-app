e54f647170fe855df617d58a2719a79d8e702a64
safarov-e <elkhan.safarov10@yandex.com>
2021-03-09 14:18:24 +0500

    постраничный вывод

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useTypedSelector} from '../hooks/useTypedSelector';
+import {useActions} from '../hooks/userActions';
 
 const TodoList = () => {
     const {page, error, loading, todos, limit} = useTypedSelector(state => state.todo)
+    const {fetchTodos, setTodoPage} = useActions()
+    const pages = [1, 2, 3, 4, 5]
+
+    useEffect(() => {
+        fetchTodos(page, limit)
+    }, [])
+
     if(loading) {
         return <h1>Идет загрузка...</h1>
     }
@@ -12,8 +20,18 @@ const TodoList = () => {
     return (
         <div>
             {todos.map(todo => {
-                return <div key={todo.id}>{todo.id} - {todo.name}</div> 
+                return <div key={todo.id}>{todo.id} - {todo.title}</div> 
             })}
+            <div style={{display: 'flex'}}>
+                {pages.map(p => {
+                    return <div
+                        onClick={() => setTodoPage(p)}
+                        style={{border: p === page ? '2px solid green' : '1px solid grey', padding: 10}}
+                    >
+                        {p}
+                    </div>
+                })}
+            </div>
         </div>
     );
 };
